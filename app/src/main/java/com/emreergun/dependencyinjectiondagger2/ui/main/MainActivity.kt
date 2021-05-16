3d6a38d9577985893fc14b8cdac2d77923c08b7c
Emre <emreergun@bgnmobi.com>
2021-05-16 10:30:03 +0300

    MainActivity added navigation drawer layout for navigation drawer

--- app/src/main/java/com/emreergun/dependencyinjectiondagger2/ui/main/MainActivity.kt
+++ app/src/main/java/com/emreergun/dependencyinjectiondagger2/ui/main/MainActivity.kt
@@ -4,36 +4,57 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.NavigationUI
 import com.emreergun.dependencyinjectiondagger2.BaseActivity
 import com.emreergun.dependencyinjectiondagger2.R
+import com.emreergun.dependencyinjectiondagger2.ui.main.profile.ProfileFragment
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : BaseActivity() {
+class MainActivity : NavigationView.OnNavigationItemSelectedListener, BaseActivity()  {
 
+    private lateinit var navView:NavigationView
+    private lateinit var drawerLayout:DrawerLayout
+    private lateinit var navController:NavController
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.profile_drawer->{
+                navController.navigate(R.id.profileFragment) // Novigat Other Fragment
+            }
+            R.id.posts_drawer->{
+                navController.navigate(R.id.postFragment) // Novigat Other Fragment
+            }
+        }
+        item.isChecked = true
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.mainToolbar))
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-
-        val navController = navHostFragment.navController
+        navView=findViewById(R.id.nav_view)
+        drawerLayout=findViewById(R.id.mainDrawerLayout)
+        initViews()
 
 
-        val appBarConfiguration = AppBarConfiguration(
-            topLevelDestinationIds = setOf(),
-            fallbackOnNavigateUpListener = ::onSupportNavigateUp
-        )
-        findViewById<Toolbar>(R.id.mainToolbar)
-            .setupWithNavController(navController, appBarConfiguration)
 
+
+    }
+
+    private fun initViews() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(navView,navController)
+        navView.setNavigationItemSelectedListener(this)
     }
 
 
@@ -58,4 +79,6 @@ class MainActivity : BaseActivity() {
     companion object {
         private const val TAG = "MainActivity"
     }
+
+
 }
